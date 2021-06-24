47971871e3b670839d015f80b52efaa2e7c885f5
Stefan Schmeltz <stefcoding@gmail.com>
2021-06-24 21:41:35 +0200

    Minor changes

--- app/src/main/java/com/example/stefan_movie_app/api/UrlCredentials.kt
+++ app/src/main/java/com/example/stefan_movie_app/api/UrlCredentials.kt
@@ -4,8 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-const val API_KEY  = "267e566a2a353cdadae0aacdb719b147"
-//const val API_KEY  = "267e566a2a353cdadae0aacdb719b147p"<--error
+const val API_KEY  = "YOUR API KEY HERE :)"
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500/"
 
